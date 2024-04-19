03918c6c976b943d59a0d21418e0206adfb0af45
Saurabh <sauravvishwakarma030@gmail.com>
2024-04-19 13:02:37 +0530

    fix

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 DATABASE_URL = ""
 REDIS_HOSTNAME = ""
 REDIS_PASSWORD = "123456789"
+=======
+DATABASE_URL = "postgresql+psycopg2://postgres:12345678@localhost/player"
+REDIS_HOSTNAME = "127.0.0.1"
+REDIS_PASSWORD = ""
+>>>>>>> 8ac8f9a (fix)
